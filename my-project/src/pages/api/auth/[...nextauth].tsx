addb79cc7b6fcad6aef50eadd705dd4331c36b9a
Rainaz <rainaz852@gmail.com>
2023-06-15 17:57:25 +0200

    another vercel try

--- my-project/src/pages/api/auth/[...nextauth].tsx
+++ my-project/src/pages/api/auth/[...nextauth].tsx
@@ -1,20 +1,28 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   // Configure one or more authentication providers
+
   providers: [
+    
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_ID!==undefined?process.env.NEXT_PUBLIC_GOOGLE_ID:"",
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET!==undefined?process.env.NEXT_PUBLIC_GOOGLE_SECRET:"",
+      clientId:
+        process.env.GOOGLE_ID !== undefined
+          ? process.env.GOOGLE_ID
+          : "",
+      clientSecret:
+        process.env.NEXTAUTH_SECRET !== undefined
+          ? process.env.NEXTAUTH_SECRET
+          : "",
+
       authorization: {
         url: "http://localhost:3000/api/auth/callback/google",
-        params: { scope: "email" }
-      }
-       
-      
+        params: { scope: "email" },
+      },
       
     }),
     // ...add more providers here
   ],
-}
-export default NextAuth(authOptions)
+  secret: "LlKq6ZtYbr+hTC073mAmAh9/h2HwMfsFo4hrfCx5mLg=",
+};
+export default NextAuth(authOptions);
