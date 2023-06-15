9de67124196be45af5458a1d1b0cc3823b63a549
Rainaz <rainaz852@gmail.com>
2023-06-15 17:37:49 +0200

    env path change

--- my-project/src/pages/api/auth/[...nextauth].tsx
+++ my-project/src/pages/api/auth/[...nextauth].tsx
@@ -4,8 +4,8 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID!==undefined?process.env.GOOGLE_ID:"",
-      clientSecret: process.env.GOOGLE_SECRET!==undefined?process.env.GOOGLE_SECRET:"",
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_ID!==undefined?process.env.NEXT_PUBLIC_GOOGLE_ID:"",
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET!==undefined?process.env.NEXT_PUBLIC_GOOGLE_SECRET:"",
       authorization: {
         url: "http://localhost:3000/api/auth/callback/google",
         params: { scope: "email" }
