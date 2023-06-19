81beb80aac75480666f27ecaaac365ca63ec3d45
Rainaz <rainaz852@gmail.com>
2023-06-19 11:28:03 +0200

    stripe connection

--- my-project/src/pages/checkout.tsx
+++ my-project/src/pages/checkout.tsx
@@ -1,17 +1,41 @@
 import { ChekoutProduct } from "@/components/ChekoutProduct";
 import { Header } from "@/components/Header";
 import { selectItems, selectTotal } from "@/slices/basketSlice";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useSelector } from "react-redux";
+
+const stirpePromise = loadStripe(process.env.stripe_public_key===undefined?"":process.env.stripe_public_key)
 
 
 
 function Checkout() {
   const session = useSession()
   const items = useSelector(selectItems)
-
   const total = useSelector(selectTotal)
+
+  const createChekoutSession = async ()=>{
+    const stripe = await stirpePromise;
+
+
+    // Call the backend to create a checout session...
+  if(session.status==="authenticated"){
+    const chekoutSession = await axios.post("/api/create-checkout-session",
+    {
+      items:items,
+      email:session.data?.user?.email,
+
+    })
+     // Redirect user to Stripe checkout
+     const result = await stripe?.redirectToCheckout({
+      sessionId:chekoutSession.data.id
+     })
+   
+  }
+
+  }
   
   return (
     <div className="bg-gray-100">
@@ -39,7 +63,7 @@ function Checkout() {
         {items.length>0 &&
         <div className="flex flex-col p-10  m-5 space-y-4 text-center  bg-white lg:w-[600px]">
           <h2 className="text-sm">Subtotal  ({items.length} products)  <span className="font-semibold">{Math.floor(total)}$</span></h2>
-        <button className={`button ${session.status==="unauthenticated"&& "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed" }`} disabled={session.status==="unauthenticated"}>
+        <button onClick={createChekoutSession} role="link" className={`button ${session.status==="unauthenticated"&& "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed" }`} disabled={session.status==="unauthenticated"}>
           {session.status==="unauthenticated"?"Sign in to checkout":"Proceed to chekout"}
         </button>
         </div>
